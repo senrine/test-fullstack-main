a9b0c25dfabcd92e4484fd81f702f579770fb9fd
Senrine <saadinesrine16@gmail.com>
2024-07-26 20:01:41 +0200

    style added

--- src/Form/ClockingDetailType.php
+++ src/Form/ClockingDetailType.php
@@ -18,11 +18,16 @@ class ClockingDetailType extends AbstractType
             ->add('project', EntityType::class, [
                 'class' => Project::class,
                 'choice_label' => 'name',
+                'attr' => ['class' => 'text-black border border-gray-200 rounded-md px-2 py-2 focus:outline-none focus:border-red w-full mb-4'],
+                'label_attr' => ['class' => 'block text-sm font-medium text-gray-400 mb-2'],
             ])
-            ->add('duration', IntegerType::class);
+            ->add('duration', IntegerType::class,[
+                'attr' => ['class' => 'text-black border border-gray-200 rounded-md px-2 py-2 focus:outline-none focus:border-red w-full mb-4'],
+                'label_attr' => ['class' => 'block text-sm font-medium text-gray-400 mb-2'],
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver):  void
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => ClockingDetail::class,
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'secondary': '#FF441B',
+        'red': "#dc3545"
+      },
+      fontFamily: {
+        'Roboto': ['Roboto', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
